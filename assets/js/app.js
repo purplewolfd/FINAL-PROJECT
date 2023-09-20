1895d03ffa2fe95c76857c4e1ea461df1efc4221
purplewolfd <106463713+purplewolfd@users.noreply.github.com>
2023-09-20 06:30:23 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+const scrolltoTop = document.querySelector('.scroll-to-top')
+window.addEventListener('scroll',() => {
+    if (window.scrollY > 300){
+        scrolltoTop.style.display = "grid" 
+    } else{
+        scrolltoTop.style.display = "none"
+    }
+
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween : 24,
+    speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 2,
+        },
+
+    }
+  });
+
+/* AOS UNIT */
+
+AOS.init();
